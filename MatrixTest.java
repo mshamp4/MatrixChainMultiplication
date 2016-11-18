0b44bc3c14575ae5b7a12ce630e51a0316434c8a
Matt <mshamp4@users.noreply.github.com>
2016-11-17 20:09:01 -0500

    Create MatrixTest.java

---
+++ MatrixTest.java
@@ -0,0 +1,16 @@
+package multiplication;
+
+public class TestMatrix {
+
+	public static void main(String[] args) {
+		
+//		Matrix A = new Matrix(2, 3, 4);
+		Matrix a = new Matrix(10, 10, 10);
+//		a.loadMatrixFile("matrix2.txt");
+		
+//		System.out.print(a.getRow());
+//		System.out.print(" " + a.getCol());
+//		System.out.println();
+		System.out.print(a);
+	}
+}
