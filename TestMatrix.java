5695fafe3f82de32c7febbfba29f3e91ad908680
Matt <mshamp4@users.noreply.github.com>
2016-11-21 23:21:17 -0500

    Update TestMatrix.java

--- TestMatrix.java
+++ TestMatrix.java
@@ -1,16 +1,40 @@
 package multiplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestMatrix {
 
 	public static void main(String[] args) {
-		
-//		Matrix A = new Matrix(2, 3, 4);
-		Matrix a = new Matrix(10, 10, 10);
-//		a.loadMatrixFile("matrix2.txt");
-		
-//		System.out.print(a.getRow());
-//		System.out.print(" " + a.getCol());
-//		System.out.println();
-		System.out.print(a);
+
+		InputStreamReader isReader = new InputStreamReader(System.in);
+		BufferedReader bufReader = new BufferedReader(isReader);
+
+		try {
+//			int[] matrixArr = {5, 1, 2, 3, 4, 5};
+//			int[] arr = {1, 2, 3, 4};
+			String input = bufReader.readLine();
+			int[] arr = new int[Integer.parseInt(input)];
+			int count = 0;
+			input = bufReader.readLine();
+
+			while (input != null) {
+				arr[count++] = Integer.parseInt(input);
+				input = bufReader.readLine();
+			}
+			
+			
+			Matrix a = new Matrix();
+			int min = a.matrixChainMultiplication(arr, 1, (arr.length-1));
+			System.out.println("Minimum number of multiplications " + min);
+
+		} catch (IOException e) {
+			System.out.println("An error has occured!");
+			System.exit(0);
+
+		} catch (NumberFormatException e) {
+			System.out.println("File must contain only integers!");
+		}
 	}
 }
